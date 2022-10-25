92219901fdeadc0757341a77ac24e8dd74601d6e
danargh <danargh86@gmail.com>
2022-10-25 20:59:11 +0700

    js day2 menambahkan stock dan amount dalam fungsi pembelian buku

--- script.js
+++ script.js
@@ -1,26 +1,63 @@
-function bookPurchase(detail, percentageDiscount, percentageTax) {
-   let { title, price, isPurchased } = detail;
+function bookPurchase(
+   detail,
+   percentageDiscount = 10,
+   percentageTax = 3,
+   stock = 10
+) {
+   let { title, price, isPurchased, amount } = detail;
+   let message;
+   let totalPrice = 0;
 
-   // count discount
-   price = (price * (100 - percentageDiscount)) / 100;
+   if (amount < stock) {
+      for (let x = 0; x < amount; x++) {
+         // total price with amount
+         totalPrice += price;
+      }
+      // count discount
+      totalPrice = (totalPrice * (100 - percentageDiscount)) / 100;
 
-   // count tax
-   price = (price * (100 - percentageTax)) / 100;
+      // count tax
+      totalPrice = (totalPrice * (100 - percentageTax)) / 100;
 
-   // cheklist purchased
-   isPurchased = true;
+      // cheklist purchased
+      isPurchased = true;
+
+      message =
+         "Terimakasih telah membayar. Stock kami masih tersedia jika anda ingin membeli lagi.";
+   } else if (amount === stock) {
+      for (let x = 0; x < amount; x++) {
+         // total price with amount
+         totalPrice += price;
+      }
+      // count discount
+      totalPrice = (totalPrice * (100 - percentageDiscount)) / 100;
+
+      // count tax
+      totalPrice = (totalPrice * (100 - percentageTax)) / 100;
+
+      // cheklist purchased
+      isPurchased = true;
+
+      message =
+         "Terimakasih telah membayar. Anda tidak bisa membeli lagi karena stock kami habis.";
+   } else {
+      message = "Maaf stock kami tidak mencukupi jumlah pembelian anda.";
+   }
 
    return {
-      title: title,
-      price: price,
-      isPurchased: isPurchased,
+      judulBuku: title,
+      Jumlah: `${amount} buku`,
+      TotalHarga: `Rp${totalPrice}`,
+      StatusBayar: isPurchased === true ? "lunas" : "belum lunas",
+      Pesan: message,
    };
 }
 
-const purchasedBook = bookPurchase(
-   { title: "Habis gelap terbitlah terang", price: 100000, isPurchased: false },
-   10,
-   3
-);
+const purchasedBook = bookPurchase({
+   title: "Habis gelap terbitlah terang",
+   price: 10000,
+   isPurchased: false,
+   amount: 10,
+});
 
 console.log(purchasedBook);
