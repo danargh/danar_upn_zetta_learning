9f2b8cb62f3ec2a0956970af63202965acb7f08e
danargh <danargh86@gmail.com>
2022-10-28 10:01:16 +0700

    ts day 2 membuat tuples

---
+++ main.ts
@@ -0,0 +1,6 @@
+function union(arr: (number | string)[]): string {
+   return arr.join(" ");
+}
+
+console.log(union([1, "data", "3", "result"]));
+console.log(union(["Bejo", "has", "4", "sport", "car"]));
