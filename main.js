ce6b87c8ff73da110b596196d623a015338f9285
danargh <danargh86@gmail.com>
2022-10-28 22:59:34 +0700

    memperbaiki detail kecil

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function union(arr) {
-    return arr.join(" ");
+   return arr.join(" ");
 }
 console.log(union([1, "data", "3", "result"]));
 console.log(union(["Bejo", "has", "4", "sport", "car"]));
