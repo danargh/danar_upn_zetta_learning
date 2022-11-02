091979e36d57467308d5d5628286b39b5e3b847c
danargh <danargh86@gmail.com>
2022-11-02 14:05:23 +0700

    membuat strukture folder aplikasi

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,9 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { TaskSearchComponent } from './task-search/task-search.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-   declarations: [AppComponent],
+   declarations: [
+      AppComponent,
+      DashboardComponent,
+      TaskDetailComponent,
+      TaskSearchComponent,
+      TasksComponent,
+      MessagesComponent,
+   ],
    imports: [BrowserModule, FormsModule],
    providers: [],
    bootstrap: [AppComponent],
