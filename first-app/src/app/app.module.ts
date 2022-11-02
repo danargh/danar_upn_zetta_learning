49c5d12e53e99c3c541a144e027a16b31caecff3
danargh <danargh86@gmail.com>
2022-11-02 11:37:23 +0700

    menambahkan module2

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { Module2Component } from './module2/module2.component';
 
 @NgModule({
-   declarations: [AppComponent],
+   declarations: [AppComponent, Module2Component],
    imports: [BrowserModule, FormsModule],
    providers: [],
    bootstrap: [AppComponent],
