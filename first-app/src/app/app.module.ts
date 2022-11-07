66513f70b58e9fcf05adba7d44e478addb0539aa
danargh <danargh86@gmail.com>
2022-11-07 10:56:00 +0700

    angular day5 implementasi service dan dependency injection

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -1,13 +1,26 @@
-import { NgModule } from '@angular/core';
+import { MaterialModule } from './module/module';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
+import { AccountsService } from './accounts.service';
+import { LoggingService } from './logging.service';
+
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatSelectModule } from '@angular/material/form-select;
+// import { MatInputModule } from '@angular/material/input';
+// import { MatListModule } from '@angular/material/list';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-   declarations: [AppComponent],
-   imports: [BrowserModule, FormsModule],
-   providers: [],
+   declarations: [AppComponent, AccountComponent, NewAccountComponent],
+   imports: [BrowserModule, FormsModule, MaterialModule],
+   // provider ditaruh parent ini agar bisa diakses dimanapun
+   providers: [AccountsService, LoggingService],
    bootstrap: [AppComponent],
 })
 export class AppModule {}
