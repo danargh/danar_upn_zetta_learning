d5426edff44103a8327d7584b87c120483d7b12f
danargh <danargh86@gmail.com>
2022-11-17 21:25:29 +0700

    angular day9 menambahkan validate di dalam form

--- first-app/src/app/user/user-input/user-input.component.ts
+++ first-app/src/app/user/user-input/user-input.component.ts
@@ -1,70 +1,92 @@
 import { Component, OnInit } from '@angular/core';
 import {
-  FormArray,
-  FormBuilder,
-  FormControl,
-  FormsModule,
-  Validators,
-  FormGroup,
+   FormArray,
+   FormBuilder,
+   FormControl,
+   FormsModule,
+   Validators,
+   FormGroup,
 } from '@angular/forms';
 import { DataService } from 'src/app/services/data.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
-  selector: 'app-user-input',
-  templateUrl: './user-input.component.html',
-  styleUrls: ['./user-input.component.scss'],
+   selector: 'app-user-input',
+   templateUrl: './user-input.component.html',
+   styleUrls: ['./user-input.component.scss'],
 })
 export class UserInputComponent implements OnInit {
-  data: {
-    Id: FormControl<number>;
-    Nama: FormControl;
-    Umur: FormControl;
-    Gender: FormControl;
-    Email: FormControl;
-    Domisili: FormControl;
-    StatusPernikahan: FormControl;
-    Alamat: FormControl;
-    KodePos: FormControl;
-    Kota: FormControl;
-    Negara: FormControl;
-  }[] = [];
+   data: {
+      Id: FormControl<number>;
+      Nama: FormControl;
+      Umur: FormControl;
+      Gender: FormControl;
+      Email: FormControl;
+      Domisili: FormControl;
+      StatusPernikahan: FormControl;
+      Alamat: FormControl;
+      KodePos: FormControl;
+      Kota: FormControl;
+      Negara: FormControl;
+   }[] = [];
+   status: any;
+   userForm = this.formBuilder.group({
+      Id: ['', [Validators.required]],
+      Nama: ['', [Validators.required, Validators.pattern('^[A-Z, a-z]*$')]],
+      Umur: [
+         '',
+         [
+            Validators.required,
+            Validators.pattern('^[0-9]*$'),
+            Validators.min(10),
+         ],
+      ],
+      Gender: ['', Validators.required],
+      Email: ['', [Validators.required, Validators.email]],
+      Domisili: [''],
+      StatusPernikahan: [''],
+      dataAlamat: new FormArray([]),
+   });
+   constructor(
+      private formBuilder: FormBuilder,
+      private dataService: DataService,
+      private activatedRoute: ActivatedRoute,
+      private router: Router
+   ) {}
+   tambahAlamat: any = (<FormArray>this.userForm.get('dataAlamat')).controls;
 
-  userForm = this.formBuilder.group({
-    Id: ['', Validators.required],
-    Nama: ['', Validators.required],
-    Umur: ['', Validators.required],
-    Gender: [''],
-    Email: ['', Validators.required],
-    Domisili: ['', Validators.required],
-    StatusPernikahan: ['', Validators.required],
-    dataAlamat: new FormArray([]),
-    KodePos: ['', Validators.required],
-    Kota: ['', Validators.required],
-    Negara: ['', Validators.required],
-  });
-  constructor(
-    private formBuilder: FormBuilder,
-    private dataService: DataService,
-    private activatedRoute: ActivatedRoute
-  ) {}
-  tambahAlamat: any = (<FormArray>this.userForm.get('dataAlamat')).controls;
+   ngOnInit(): void {
+      this.data = this.dataService.data;
+      this.userForm.statusChanges.subscribe((change) => {
+         this.status = change;
+         console.log(this.status);
+      });
+   }
 
-  ngOnInit(): void {
-    this.data = this.dataService.data;
-  }
-
-  tambah() {
-    this.dataService.addData(this.userForm.value, false);
-  }
-  tambahAlamatBaru() {
-    (<FormArray>this.userForm.get('dataAlamat')).push(
-      new FormGroup({
-        Alamat: new FormControl(null),
-        KodePos: new FormControl(null),
-        Kota: new FormControl(null),
-        Negara: new FormControl(null),
-      })
-    );
-  }
+   tambah() {
+      if (this.status == 'INVALID') {
+         Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Data tidak Valid',
+         });
+      } else {
+         Swal.fire('New data has been added!');
+         setTimeout(() => {
+            this.dataService.addData(this.userForm.value, false);
+            this.router.navigate(['/users/user-list']);
+         }, 2000);
+      }
+   }
+   tambahAlamatBaru() {
+      (<FormArray>this.userForm.get('dataAlamat')).push(
+         new FormGroup({
+            Alamat: new FormControl(null),
+            KodePos: new FormControl(null),
+            Kota: new FormControl(null),
+            Negara: new FormControl(null),
+         })
+      );
+   }
 }
