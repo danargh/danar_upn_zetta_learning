71a167821e16c5bfe962d3dc86c314ca1237ffb7
danargh <danargh86@gmail.com>
2022-11-17 21:04:37 +0700

    angular day7 membuat form control

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -1,12 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { UserComponent } from './user/user.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserInputComponent } from './user/user-input/user-input.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { HomeComponent } from './home/home.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './module/module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
-   declarations: [AppComponent],
-   imports: [BrowserModule, FormsModule],
+   declarations: [
+      AppComponent,
+      UserComponent,
+      UserListComponent,
+      UserInputComponent,
+      UserEditComponent,
+      HomeComponent,
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      FormsModule,
+      ReactiveFormsModule,
+      MaterialModule,
+      BrowserAnimationsModule,
+   ],
    providers: [],
    bootstrap: [AppComponent],
 })
